85efcec5d18660d6fce0522fe05b6d45e4c2da79
myk3l <ayomikesam@gmail.com>
2026-03-07 18:32:13 +0100

    merge branch

---
+++ src/models/order.model.js
@@ -0,0 +1,34 @@
+
+import { DataTypes } from "sequelize";
+import sequelize from "./src/MySql/config/db.js";
+
+
+const Order = sequelize.define(
+  'Order',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    dateOrdered: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    orderStatus: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }
+);
+
+export default Order;
---
+++ src/models/order_items.model.js
@@ -0,0 +1,29 @@
+
+import { DataTypes } from "sequelize";
+import sequelize from "./src/MySql/config/db.js";
+
+const OrderItem = sequelize.define(
+  'OrderItem',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    priceAtPurchase: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    }
+  }
+);
+
+export default OrderItem
