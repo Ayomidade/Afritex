0bf995058780663975f1c4d8dbd6a0bd8776d447
Stanley Nelson Azubuike <stanleymailbag@gmail.com>
2026-03-24 14:41:21 +0100

    fix: remove hardcoded credentials, use env variables

--- src/MySql/config/db.js
+++ src/MySql/config/db.js
@@ -4,8 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  `mysql://root:kUyVsOAVHdksExrEhrCfMMVPNqQSRcyT@centerbeam.proxy.rlwy.net:27320/railway`,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
     pool: {
