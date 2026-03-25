643cb07d2e9dae1c522c863c4df8172b535ede49
Samuel Michael <89843000+Ayomidade@users.noreply.github.com>
2026-03-25 07:47:42 +0100

    Update db.js
    fix: Updated the db.js

--- src/MySql/config/db.js
+++ src/MySql/config/db.js
@@ -9,7 +9,18 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      connectTimeout: 60000,
+    },
   }
 );
 
